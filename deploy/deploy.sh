93d5c53dd59af5f646dbeb070051070d47a7a79a
Matthew Fenwick <mfenwick100@gmail.com>
2020-04-05 07:12:02 -0400

    add expose instructions

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,3 +3,8 @@ NS=$1
 kubectl create ns $NS
 
 kubectl create -n $NS -f server.yaml
+
+# kubectl expose deployment -n $NS up-and-down-the-river --name uad-exposed --type LoadBalancer --port 5932 --target-port 5932
+# IP=$(kubectl get nodes -o json | jq -r '.items[0].status.addresses | map(select(.type == "ExternalIP"))[0].address')
+# PORT=$(kubectl get svc -n upand uad-exposed -o json | jq '.spec.ports[0].nodePort')
+# curl http://$IP:$PORT/model
