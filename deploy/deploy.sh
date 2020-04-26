7a7ad2d2239fc567951fdc4be0aa932871a81351
Matthew Fenwick <mfenwick100@gmail.com>
2020-04-26 14:19:59 -0400

    parameterize image tag in deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,9 @@
 NS=$1
+IMAGE_TAG=$2
 
 kubectl create ns $NS
 
-kubectl create -n $NS -f server.yaml
+sed "s/\$IMAGE_TAG/$IMAGE_TAG/g" server.yaml | kubectl create -f - -n $NS
 
 kubectl expose deployment -n $NS up-and-down-the-river --name uad-exposed --type LoadBalancer --port 5932 --target-port 5932
 IP=$(kubectl get nodes -o json | jq -r '.items[0].status.addresses | map(select(.type == "ExternalIP"))[0].address')
